3e9bf46829b6aff190bc73587afe1b8485268144
“David <dcohen@memphis.edu>
2015-09-12 14:31:10 -0500

    update front end and css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,19 @@
 //= require turbolinks
 //= require bootstrap/dist/js/bootstrap
 //= require_tree .
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+
