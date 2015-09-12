d44b49c6bffe7bcb4fa251fe9074e1b5f40adb9a
“David <dcohen@memphis.edu>
2015-09-12 15:00:24 -0500

    Move javascript to custom.js file and did things related and stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,24 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require custom
 //= require jquery/dist/jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap/dist/js/bootstrap
 //= require_tree .
-
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
 
 
